473b90b82e8ce2aeba5bf7294dc94477f47a8380
ignatio <bigbrisco@gmail.com>
2022-06-13 01:45:24 -0400

    added explosions
    explosions added, bullets explode on terrain and at target.

--- Bullet.gd
+++ Bullet.gd
@@ -21,7 +21,26 @@ func is_outside_view_bounds():
 	return position.x>OS.get_screen_size().x or position.x<0.0 or position.y>OS.get_screen_size().y or position.y<0.0
 
 
-#func _on_Bullet_body_entered(body):
-#	if body.get_collision_layer_bit(2):
-#		body.hit_by_bullet(body.position)
-#	queue_free()
+#func _on_Bullet_area_entered(area):
+#	queue_free() # Replace with function body.
+
+
+func _on_Bullet_body_entered(body):
+	if body.is_in_group("targetable"):
+		$Sprite.visible = false
+		$explosion.set_global_rotation_degrees(0)
+		$explosion.play()
+		bulletSpeed = 0
+		#queue_free() # Replace with function body.
+
+
+func _on_explosion_animation_finished():
+	queue_free() # Replace with function body.
+
+
+func _on_Bullet_area_entered(area):
+	if area.is_in_group("targetable"):
+		$Sprite.visible = false
+		$explosion.set_global_rotation_degrees(0)
+		$explosion.play()
+		bulletSpeed = 0
--- Turret.gd
+++ Turret.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 var angular_speed = 5.0
 
-onready var bullet = preload("res://Bullet.tscn")
+onready var bullet = preload("res://prefabs/Bullet.tscn")
 onready var muzzle = $Position2D
 
 func _physics_process(delta):
@@ -16,13 +16,16 @@ func _physics_process(delta):
 		angular_speed = abs(angle / .3)
 	else:
 		angular_speed = 1
-		
+	
 func _process(delta):
 	
 	if Input.is_action_just_pressed("tank_fire"):
 		var shot = bullet.instance()
 		owner.add_child(shot)
 		shot.transform = muzzle.global_transform
+	
+	$Aimer.set_global_rotation_degrees(0)
+	$Aimer.position = get_local_mouse_position()
 
 	
 	
